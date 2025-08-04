48f032c066102f7feec308831fb3b4a05201b7b4
Huy Duong <huy.duongdinh@gmail.com>
2025-08-04 23:21:27 +0700

    UPdate

--- users/dinhhuy258/rules.mk
+++ users/dinhhuy258/rules.mk
@@ -6,12 +6,8 @@ SRC += $(USER_PATH)/features/swapper.c
 SRC += $(USER_PATH)/features/taphold.c
 SRC += $(USER_PATH)/features/caps_word.c
 SRC += $(USER_PATH)/features/led_indicators.c
-
-# Secret macro system
 SRC += $(USER_PATH)/features/secret_triggers.c
-ifneq (,$(wildcard $(USER_PATH)/features/secrets.c))
-    SRC += $(USER_PATH)/features/secrets.c
-endif
+SRC += $(USER_PATH)/features/secrets.c
 
 # https://github.com/qmk/qmk_firmware/issues/21137#issuecomment-1577898767
 INTROSPECTION_KEYMAP_C = $(USER_PATH)/keymap_introspection.c
