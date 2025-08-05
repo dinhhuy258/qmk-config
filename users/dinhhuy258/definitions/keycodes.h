502ce2db41a5f8ea3c77803254b482fe5ae2e685
Huy Duong <huy.duongdinh@gmail.com>
2025-08-05 20:00:11 +0700

    feat(secrets): add support for more secret keys
    - Expands the number of definable secret keys from 1 to 3.
    
    - Adds new keycodes `SECRET_2` and `SECRET_3`.
    
    - Implements the logic to handle the new secret keycodes.

--- users/dinhhuy258/definitions/keycodes.h
+++ users/dinhhuy258/definitions/keycodes.h
@@ -59,4 +59,6 @@ enum keycodes {
     SWITCH_APP = SAFE_RANGE, // Switch to next window (cmd-tab)
     JIGGLE,
     SECRET_1,
+    SECRET_2,
+    SECRET_3,
 };
--- users/dinhhuy258/features/secrets.c
+++ users/dinhhuy258/features/secrets.c
@@ -10,10 +10,18 @@
 #ifndef SECRET_1_VALUE
 #    define SECRET_1_VALUE ""
 #endif
+#ifndef SECRET_2_VALUE
+#    define SECRET_2_VALUE ""
+#endif
+#ifndef SECRET_3_VALUE
+#    define SECRET_3_VALUE ""
+#endif
 
 const char secret_1[] PROGMEM = SECRET_1_VALUE;
+const char secret_2[] PROGMEM = SECRET_2_VALUE;
+const char secret_3[] PROGMEM = SECRET_3_VALUE;
 
-static const char *const secrets[] PROGMEM = {secret_1};
+static const char *const secrets[] PROGMEM = {secret_1, secret_2, secret_3};
 
 void process_secret_macro(uint16_t keycode, keyrecord_t *record) {
     clear_mods();
@@ -31,6 +39,20 @@ bool process_secret_triggers(uint16_t keycode, keyrecord_t *record) {
             case KC_Q:
                 if (record->event.pressed) {
                     process_secret_macro(SECRET_1, record);
+                }
+                secret_mode_deactivate();
+
+                return false; // Consume the key
+            case KC_W:
+                if (record->event.pressed) {
+                    process_secret_macro(SECRET_2, record);
+                }
+                secret_mode_deactivate();
+
+                return false; // Consume the key
+            case KC_E:
+                if (record->event.pressed) {
+                    process_secret_macro(SECRET_3, record);
                 }
                 secret_mode_deactivate();
 
